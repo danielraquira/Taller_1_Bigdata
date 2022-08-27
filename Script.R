21b357ec1896986ec7a8cd7bb839e6fe71424915
danielraquira <df.raquira@uniandes.edu.co>
2022-08-27 16:49:44 -0500

    Agrego mi codigo y parte del script.

--- Script.R
+++ Script.R
@@ -1,1 +1,14 @@
-2+2 ##suma
+###############################################
+#            Problem set 1                    #
+#                                             #
+#Colaboradores:                               #
+# Daniel Raquira 201914059                    #
+# Santiago Becerra 201911587                  #
+#                                             #
+###############################################
+
+rm(list=ls())
+require(pacman)
+p_load(tidyverse, rvest)
+url1 <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html"
+html1 <- read_html(url1)
